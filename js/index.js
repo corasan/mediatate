549c6c4d3ea186cd933fb3aa6bfe6e03dd783171
Henry Paulino <henrypl360@gmail.com>
2016-11-17 18:44:21 -0500

    phase 5 complete

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+  $('#submit-form').click(() => {
+    document.getElementById('form').style.display='none';
+    document.getElementById('thank-you').style.display='flex';
+  });
+});
