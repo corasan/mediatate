19d03d90521c4045b782b531e143e19e1dc2f0c0
Henry Paulino <henrypl360@gmail.com>
2016-11-18 16:17:34 -0500

    slideshow js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
+const images = document.querySelectorAll('.images');
+const nextSlide = document.getElementById('next-slide');
+const previousSlide = document.getElementById('previous-slide');
+const showSlide = document.getElementById('show-slide');
+
 window.onload = (() => {
   // delayer();
-  images();
+  loadImages();
   // testGroup();
-  submitForm();
+  showSlide.src = images[0].src
 });
 
-const submitForm = () => {
-  $('#submit-form').click(() => {
-    document.getElementById('form').style.display='none';
-    document.getElementById('thank-you').style.display='flex';
-  });
-}
+document.getElementById('submit-form').addEventListener('click', () => {
+  document.getElementById('form').style.display='none';
+  document.getElementById('thank-you').style.display='flex';
+});
+
 
 const testGroup = () => {
   const arr = ['Variation', 'Control'];
@@ -18,17 +22,35 @@ const testGroup = () => {
   const group = arr[i];
 
   if(group === 'Control') {
-    $('#myModal').modal('show');
+    document.getElementById('myModal').modal('show');
     document.getElementById('form-container').style.display='none';
   }
 }
 
-const images = () => {
-  let url = "https://s-media-cache-ak0.pinimg.com/736x/4b/fe/44/4bfe443c1db09c0943de27164783e4fc.jpg";
+// Load images with version number
+const loadImages = () => {
+  let url = 'https://s-media-cache-ak0.pinimg.com/736x/4b/fe/44/4bfe443c1db09c0943de27164783e4fc.jpg';
   let d = new Date();
-  document.querySelectorAll(".images").forEach((image, i) => {
-    image.src = `${url}?ver=${i + '-' + d.getTime()}`
+  images.forEach((image, i) => {
+    image.src = `${image.src}?ver=${i + '-' + d.getTime()}`
   });
+}
+
+// Slideshow
+nextSlide.addEventListener('click', () => slideShow(1));
+previousSlide.addEventListener('click', () => slideShow(-1));
+
+let current = 0;
+let imagesLength = images.length -1;
+
+// num should be a 1 or -1
+slideShow = (num) => {
+  console.log('Current Slide:', current);
+  current = current + num;
+  if (current > imagesLength) current = 0;
+  if (current < 0) current = imagesLength;
+
+  showSlide.src = images[current].src;
 }
 
 // Functions from script tags
