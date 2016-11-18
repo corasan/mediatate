33597db072f91b802ec0fb06d9c25ab3cbfba768
Henry Paulino <henrypl360@gmail.com>
2016-11-17 19:34:45 -0500

    phase 4 complete

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 $(document).ready(() => {
+  testGroup();
+  submitForm();
+});
+
+const submitForm = () => {
   $('#submit-form').click(() => {
     document.getElementById('form').style.display='none';
     document.getElementById('thank-you').style.display='flex';
   });
-});
+}
+
+const testGroup = () => {
+  const arr = ['Variation', 'Control'];
+  const i = Math.round(Math.random());
+  const group = arr[i];
+
+  if(group === 'Control') $('#myModal').modal('show');
+}
