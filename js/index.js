0321b44b0dec10805ae1cc912b13cc34d59ec8fa
Henry Paulino <henrypl360@gmail.com>
2016-11-17 20:51:59 -0500

    style changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-$(document).ready(() => {
+window.onload = (() => {
+  // delayer();
+  images();
   testGroup();
   submitForm();
 });
@@ -15,5 +17,34 @@ const testGroup = () => {
   const i = Math.round(Math.random());
   const group = arr[i];
 
-  if(group === 'Control') $('#myModal').modal('show');
+  if(group === 'Control') {
+    $('#myModal').modal('show');
+    document.getElementById('form-container').style.display='none';
+  }
+}
+
+const images = () => {
+  let url = "https://s-media-cache-ak0.pinimg.com/736x/4b/fe/44/4bfe443c1db09c0943de27164783e4fc.jpg";
+  let d = new Date();
+  document.querySelectorAll(".images").forEach((image, i) => {
+    image.src = `${url}?ver=${i + '-' + d.getTime()}`
+  });
+}
+
+// Functions from script tags
+const important = () => {
+  delayer();
+}
+
+const sleep = (milliseconds) => {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+const delayer = () => {
+  sleep(7000);
 }
