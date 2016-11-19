fce4ac349ff562338f49797c36c02c2957eebc27
Henry Paulino <henrypl360@gmail.com>
2016-11-19 17:39:21 -0500

    groups

--- js/index.js
+++ js/index.js
@@ -1,20 +1,41 @@
-window.onload = (() => {
-  delayer();
-  images();
-  testGroup();
-  submitForm();
-});
+const slideImages = document.querySelectorAll('.slide-images');
+const images = document.querySelectorAll('.images');
+const nextSlide = document.getElementById('next-slide');
+const previousSlide = document.getElementById('previous-slide');
+const showSlide = document.getElementById('show-slide');
+const like1 = document.getElementById('like-1');
+const like2 = document.getElementById('like-2');
 
-const submitForm = () => {
-  $('#submit-form').click(() => {
+window.onload = (() => {
+  if (testGroup()) {
+    loadImages(slideImages);
+    thumb.style.color = 'white';
+  }
+  loadImages(images);
+  important();
+  showSlide.src = slideImages[0].src
+
+  // Like button
+  like1.addEventListener('click', () => {
+    like1.style.color === 'black' ? like1.style.color = 'royalblue' : like1.style.color = 'black';
+  });
+
+  like2.addEventListener('click', () => {
+    like2.style.color === 'black' ? like2.style.color = 'royalblue' : like2.style.color = 'black';
+  });
+
+  document.getElementById('like-slide').addEventListener('click', () => alert(`You liked this slide`))
+
+
+  // Button to submit form
+  document.getElementById('submit-form').addEventListener('click', () => {
     document.getElementById('form').style.display='none';
     document.getElementById('thank-you').style.display='flex';
   });
-}
 
-document.getElementById('thumbs-up').addEventListener('click', () => {
-  console.log('clicked');
-  alert(`You liked this picture!`);
+  // Slideshow buttons
+  nextSlide.addEventListener('click', () => slideShow(1));
+  previousSlide.addEventListener('click', () => slideShow(-1));
 });
 
 const testGroup = () => {
@@ -22,18 +43,32 @@ const testGroup = () => {
   const i = Math.round(Math.random());
   const group = arr[i];
 
-  if(group === 'Control') {
+  if(group === 'Variation') {
+    document.getElementById('variation-group').style.display = 'block'
     $('#myModal').modal('show');
-    document.getElementById('form-container').style.display='none';
+  } else {
+    document.getElementById('control-group').style.display = 'block'
   }
 }
 
-const images = () => {
-  let url = "https://s-media-cache-ak0.pinimg.com/736x/4b/fe/44/4bfe443c1db09c0943de27164783e4fc.jpg";
+// Load images with version number
+const loadImages = (imgs) => {
   let d = new Date();
-  document.querySelectorAll(".images").forEach((image, i) => {
-    image.src = `${url}?ver=${i + '-' + d.getTime()}`
+  imgs.forEach((image, i) => {
+    image.src = `${image.src}?ver=${i + '-' + d.getTime()}`
   });
+}
+
+let current = 0;
+let imagesLength = images.length -1;
+// num should be a 1 or -1
+slideShow = (num) => {
+  console.log('Current Slide:', current);
+  current = current + num;
+  if (current > imagesLength) current = 0;
+  if (current < 0) current = imagesLength;
+
+  showSlide.src = slideImages[current].src;
 }
 
 // Functions from script tags
